93b22606fddcd211bebaf4a0a68841e32d72cca6
Gwyneth-Chen <43485598+Gwyneth-Chen@users.noreply.github.com>
2019-02-09 01:09:04 -0500

    Update smmry-algorithm.py

--- smmry-algorithm.py
+++ smmry-algorithm.py
@@ -2,7 +2,6 @@ import re
 import requests
 from dictionary import print_dict
 
-# assuming I have a dictionary of words and their absolute frequencies
 frequencies = print_dict()
 maxFrequency = max(frequencies.values())
 
@@ -24,21 +23,17 @@ pointValues = dict()
 for key in frequencies:
     pointValues[key] = categories[max(k for k in categories.keys() if frequencies[key] >= k)]
 
-
-# haven't ignored forms of address as detailed; shouldn't be a problem in 
-# EULAs but should figure out
 # currently requires minimum sentence length 3 non-punctuation chars; likely 
 # sufficient for EULA; elsewhere will remove abbreviated English honorifics
 regexp = re.compile(r'[A-Za-z,;\-\'"\s\d]{3,}[.?!]', re.MULTILINE | re.DOTALL)
 
-"""Given an input text, returns the 
+""" Given an input text, returns a list of up to 20 of the longest and/or most obscurely-worded 
+    sentences in the text
 """
 def summarize(input_text):
     text = input_text
     sentences = re.findall(regexp, text)
 
-    # need to maintain the chronological order here
-    # kinda ok to be a little inefficient?? Using py anyway
     sentenceValues = []
     for s in sentences:
         words = re.findall(r'[A-Za-z\']+', s)
@@ -48,21 +43,13 @@ def summarize(input_text):
             if w in pointValues:
                 sValue = sValue + pointValues[w]
         sentenceValues.append(sValue)
-    maxIndices = sorted(range(len(sentenceValues)), key=lambda i: sentenceValues[i])[-min(int(len(sentences)/20 + 1), 5):]
+    maxIndices = sorted(range(len(sentenceValues)), key=lambda i: sentenceValues[i])[-min(int(len(sentences)/20 + 1), 20):]
 
     smmry = []
     for i in range(len(maxIndices)):
         smmry.append(sentences[maxIndices[i]])
 
     return smmry
-
-# random text test data; sentences are inordinately long
-# linkf = "https://www.cs.cmu.edu/~spok/grimmtmp/001.txt"
-# f = requests.get(linkf)
-# linkh = "https://www.cs.cmu.edu/~spok/grimmtmp/002.txt"
-# h = requests.get(linkh)
-# linkh = "https://www.cs.cmu.edu/~spok/grimmtmp/003.txt"
-# k = requests.get(linkk)
 
 linkg = "https://www.google.com/chrome/privacy/eula_text.html"
 g = requests.get(linkg)
