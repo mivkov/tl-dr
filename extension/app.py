b86e85f98429615ad9a0c0385b5cff4c31eb8d02
sakshir0 <32418305+sakshir0@users.noreply.github.com>
2019-02-08 23:19:13 -0500

    Add files via upload
    Server code

---
+++ extension/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_session import MongoDBSessionInterface
+from pymongo import MongoClient
+
+
+app = Flask(__name__)
+app.secret_key = "SECRET KEY"
+app.config.from_object(__name__)
+CORS(app)
+
+client = MongoClient()
+db = client.database
+app.session_interface = MongoDBSessionInterface(client=client,
+                                                db="flask_sessions",
+                                                collection="sessions",
+                                                key_prefix=app.secret_key,
+                                                use_signer=True)
+
+# HTTP Errors handlers
+@app.errorhandler(404)
+def url_error(e):
+    return "404 Error: Page Not Found"
+@app.errorhandler(500)
+def server_error(e):
+    return "500 Error: Internal Server Error"
+
+# API route
+@app.route('/', methods = ['POST'])
+def api():
+	data = request.data
+	return data
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', debug=True)
